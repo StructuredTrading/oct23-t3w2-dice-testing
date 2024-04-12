73db90a0a58a66af040d10698721342139d89489
StructuredTrading <samowen1992@hotmail.com>
2024-04-13 09:38:58 +1000

    feat: impliment rolling dice with advantage.

--- src/diceRolling.js
+++ src/diceRolling.js
@@ -1,12 +1,31 @@
 
 
-function rollDice(diceSize = 6){
+function rollDice(diceSize = 6) {
 	let rollResult = Math.floor(Math.random() * diceSize) + 1
 
 	return rollResult;
 }
 
 
+function rollDiceWithAdvantage(diceSize = 6) {
+    let result = {
+        finalResult: null,
+        rolls: []
+    }
+
+    result.rolls = [
+        rollDice(diceSize),
+        rollDice(diceSize)
+    ];
+
+
+    result.finalResult = Math.max(...result.rolls);
+
+
+    return result;
+}
+
 module.exports = {
-	rollDice
+	rollDice,
+    rollDiceWithAdvantage
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { rollDice } = require("./diceRolling");
+const { rollDice, rollDiceWithAdvantage } = require("./diceRolling");
 
 
 console.log(rollDice());
@@ -6,3 +6,5 @@ console.log(rollDice());
 console.log(rollDice(20));
 
 console.log(rollDice(12));
+
+console.log(rollDiceWithAdvantage(20));
